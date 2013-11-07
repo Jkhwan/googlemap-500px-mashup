6b99b04140657794672035e9f457d1348ef3c7b8
viabledish <viable.dish@gmail.com>
2013-11-07 23:48:42 +0000

    additional api call

--- js/app.js
+++ js/app.js
@@ -18,6 +18,8 @@ $(function () {
 		      
 		  }
 		});*/
+		//Using getElementById bc of GoogleMaps documentation
+		var mapDiv = document.getElementById('map-canvas');
 
 		var mapOptions = {
 		  center: new google.maps.LatLng(49.261226, -123.1139268),
@@ -28,17 +30,20 @@ $(function () {
 		map = new google.maps.Map(document.getElementById("map-canvas"),
 		    mapOptions);
 
+		google.maps.event.addDomListener(mapDiv, 'mouseup', plot500);
+		plot500();
 	}
 
 	google.maps.event.addDomListener(window, 'load', initialize);
 
+	var markers = [];
 	var urlLat = '49.261226';
 	var urlLong = '-123.11392628';
 	var consumerKey = 'Ji9CYX99WEZ4A11GWcbQkwJ32MGDttQlsKBiMs35';
 	var imageSize = '4';
 	var radius = '1km';
 	var rpp = '100';
-	var url = 'https://api.500px.com/v1/photos/search?geo=' + urlLat + ',' + urlLong + ',' + radius + '&rpp=' + rpp + '&image_size=' + imageSize + '&consumer_key=' + consumerKey;
+	var url = '';
 	
 	function Image(latitude, longitude, url) {
 		this.latitude = latitude;
@@ -46,25 +51,37 @@ $(function () {
 		this.url = url;
 	};
 
-	$.getJSON(url, 
-		function ( data ) {
-			var imageCollection = [];
+	var plot500 = function() {
+		urlLat = map.getCenter().nb;
+		urlLong = map.getCenter().ob;
+		console.log(urlLat);
+		console.log(urlLong);
+		url = 'https://api.500px.com/v1/photos/search?geo=' + urlLat + ',' + urlLong + ',' + radius + '&rpp=' + rpp + '&image_size=' + imageSize + '&consumer_key=' + consumerKey;
+		console.log(url);
+		
+		$.getJSON(url, 
+			function ( data ) {
+				var imageCollection = [];
 
-			$( data.photos ).each(function (index, photo) {
-				var image = new Image(photo.latitude, photo.longitude, photo.image_url)
-				imageCollection.push(image);			
-			});
-			console.log(imageCollection);
+				$( data.photos ).each(function (index, photo) {
+					var image = new Image(photo.latitude, photo.longitude, photo.image_url)
+					imageCollection.push(image);			
+				});
+				console.log(imageCollection);
 
-			$(imageCollection).each(function(index, pin) {
-				var myLatlng = new google.maps.LatLng(pin.latitude, pin.longitude);
-				var marker = new google.maps.Marker({
+				$(imageCollection).each(function(index, pin) {
+					var myLatlng = new google.maps.LatLng(pin.latitude, pin.longitude);
+					var marker = new google.maps.Marker({
 
-		      position: myLatlng,
-		      map: map,
-		      //title: 'Hello World!'
-		  	});
-			});
+				      position: myLatlng,
+				      map: map,
+				      //title: 'Hello World!'
+			  		});
+			  		markers.push(marker);
+				});
 
-		}
-)});
+			}
+		)
+	}
+	
+});
